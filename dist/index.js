1aa93b651a56fb8152d2ea8e6da054d9d46de427
Moin Khan <albertkhan122@gmail.com>
2026-02-20 00:03:16 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `hello ${person}, welcome to world`;
+}
+const username = "hello to typescritp";
+console.log(greet(username));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string {
+    return `hello ${person}, welcome to world`
+}
+
+const username: string = "hello to typescritp"
+console.log(greet(username));
